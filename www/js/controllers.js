c9e23c749fd5bf153a15b6853074fa3642da66e7
jeremymcintyre & Rex Salisbury <pair+jeremymcintyre.resalisbury@devbootcamp.com>
2015-02-14 17:42:51 -0800

    Update login.html to have a button for login with Google; remove unecessary code from the AppCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,28 +21,37 @@ angular.module('starter.controllers', [])
     $scope.modal.show();
   };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+  // Oauth Login
+  $scope.authenticate = function(platform){
+    console.log("we about to authenticate!!!")
   };
+
+  // Perform the login action when the user submits the login form
+  // $scope.doLogin = function() {
+  //   console.log('Doing login', $scope.loginData);
+
+  //   // Simulate a login delay. Remove this and replace with your login
+  //   // code if using a login system
+  //   $timeout(function() {
+  //     $scope.closeLogin();
+  //   }, 1000);
+  // };
 })
 
 .controller('NewMessageCtrl', function($scope, $http) {
   $scope.message = {};
 
-  $scope.scheduleMessage = function(message){ 
+  $scope.scheduleMessage = function(message){
     console.log(message);
+    var data = {
+      number: message.contact
+    };
+  debugger
     // post route to backend
     var req = {
       method: 'POST',
-      url: 'http://localhost:8100/messages',
-      data: message
+      url: 'http://localhost:3000/users/1/send_verification_code',
+      data: data
     }
 
     $http(req)
