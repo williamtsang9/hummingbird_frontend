48d9174ddccc9e0ff39d90ed471d1bba7c5b6ab0
William Tsang <williamkt91@gmail.com>
2015-02-14 16:21:11 -0800

    Added Http request post route to new message. Have not tested with backend.

---
+++ www/js/controllers.js
@@ -0,0 +1,52 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('NewMessageCtrl', function($scope, $http) {
+  $scope.message = {};
+
+  $scope.scheduleMessage = function(message){ 
+    console.log(message);
+    // post route to backend
+    var req = {
+      method: 'POST',
+      url: 'http://localhost:8100/messages',
+      data: message
+    }
+
+    $http(req)
+      .success(function(response){console.log(response)})
+      .error(function(response){console.log(response)});
+  }
+})
